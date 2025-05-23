4f2cd4ca9358124ff87c9c4f23881f8f6c995033
joel alexander <wksx9d@gmail.com>
2025-05-22 23:04:35 -0500

    Definir modelo de datos ProductModel según la estructura de la API

---
+++ app/src/main/java/com/example/app_api/data/model/ProductModel.kt
@@ -0,0 +1,11 @@
+package com.example.app_api.data.model
+
+data class ProductModel(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String
+)
+
